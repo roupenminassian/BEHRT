e58625d79dd8a48f7128e3f155e3f11a0c81217e
Roupen Minassian <61346944+roupenminassian@users.noreply.github.com>
2023-12-18 09:45:42 +1100

    Update

--- dataLoader/NumericalTrain.py
+++ dataLoader/NumericalTrain.py
@@ -1,70 +1,84 @@
-from torch.utils.data.dataset import Dataset
-import torch
-import numpy as np
+import ast
 import pandas as pd
+import numpy as np
+import torch
+from torch.utils.data import Dataset
 
 class NTLoader(Dataset):
     def __init__(self, dataframe, max_len):
         self.max_len = max_len
-        self.data = dataframe.applymap(self.convert_to_float)
-        self.features = ['HR', 'Temp', 'O2Sat', 'Resp']
+        self.features = ['HR', 'Temp', 'O2Sat', 'Resp']  # Update this list based on actual features
         self.label_col = 'SepsisLabel'
-        self.normalized_data = self.normalize_data(self.data)
+        self.dataframe = dataframe
 
-    def convert_to_float(self, x):
-        try:
-            return float(x)
-        except ValueError:
-            return np.nan  # Convert non-numeric values to NaN
+        # Parse and align sequences
+        self.parsed_data = self.parse_and_align_sequences(self.dataframe)
+
+        # Normalize the feature columns
+        self.normalized_data = self.normalize_data(self.parsed_data)
+
+    def parse_and_align_sequences(self, df):
+        parsed_df = pd.DataFrame(index=df.index)
+        for col in df.columns:
+            if col in self.features or col == self.label_col:
+                parsed_df[col] = df[col].apply(lambda x: self.parse_dict(x))
+        aligned_df = pd.DataFrame(index=df.index)
+        for col in parsed_df.columns:
+            aligned_df[col] = parsed_df[col].apply(lambda x: self.align_sequence(x, self.max_len))
+        return aligned_df
+
+    def parse_dict(self, dict_str):
+        if pd.isna(dict_str):
+            return {}
+        else:
+            return ast.literal_eval(dict_str)
+
+    def align_sequence(self, sequence, max_len):
+        if not sequence:
+            return [np.nan] * max_len
+        else:
+            sorted_sequence = [sequence[k] for k in sorted(sequence)]
+            return sorted_sequence + [np.nan] * (max_len - len(sorted_sequence))
 
     def normalize_data(self, df):
         df_normalized = df.copy()
+
+        # Find the global maximum and minimum values for each feature
+        global_max = {feature: np.nanmax(np.concatenate(df[feature].values)) for feature in self.features}
+        global_min = {feature: np.nanmin(np.concatenate(df[feature].values)) for feature in self.features}
+
+        # Normalize each feature based on the global max and min
         for feature in self.features:
-            df_normalized[feature] = df[feature].apply(lambda x: self.normalize_sequence(x) if isinstance(x, list) else x)
+            max_val = global_max[feature]
+            min_val = global_min[feature]
+            df_normalized[feature] = df_normalized[feature].apply(
+                lambda x: [(v - min_val) / (max_val - min_val) if not np.isnan(v) else 0 for v in x]
+            )
+
         return df_normalized
-
-    def normalize_sequence(self, sequence):
-        # Convert to numeric and filter out non-numeric values
-        numeric_sequence = pd.to_numeric(sequence, errors='coerce').dropna()
-
-        if len(numeric_sequence) == 0:
-            return []
-
-        max_val = numeric_sequence.max()
-        min_val = numeric_sequence.min()
-
-        # Normalization
-        return [(x - min_val) / (max_val - min_val) if max_val != min_val else 0 for x in numeric_sequence]
-
 
     def __getitem__(self, index):
         patient_data = self.normalized_data.iloc[index]
-        feature_sequences = [self.pad_sequence(patient_data[feature], 50) for feature in self.features]
-        # Ensure the features tensor is of shape (batch_size, sequence_length, num_features)
-        features_tensor = torch.tensor(feature_sequences, dtype=torch.float).transpose(0, 1)
+        features = [patient_data[feature] for feature in self.features]
+        labels = patient_data[self.label_col]
 
-        label_sequence = self.pad_sequence(patient_data[self.label_col], pad_value=0)
+        features_tensor = torch.tensor(features, dtype=torch.float).transpose(0, 1)
 
-        # Convert to tensor
-        try:
-            label_tensor = torch.tensor(label_sequence, dtype=torch.long)
-        except RuntimeError as e:
-            print("Error with label sequence:", label_sequence)
-            raise e
+        # Replace nan values with 0 in label sequence
+        labels = [0 if pd.isna(x) else x for x in labels]
 
-        return features_tensor, label_tensor
+        # Debugging section for labels and normalized features
+        print("Original label sequence:", self.dataframe[self.label_col].iloc[index])
+        print("Processed label sequence before tensor conversion:", labels)
+        print("Normalized features:", features)
+        
+        labels_tensor = torch.tensor(labels, dtype=torch.long)
 
-    def pad_sequence(self, sequence, pad_value=0):
-        # Check if the input is a single value; if so, convert it to a list
-        if isinstance(sequence, (float, int, np.float64)):
-            sequence = [sequence]
-
-        # Replace 'nan' with pad_value in the sequence
-        cleaned_sequence = [x if not np.isnan(x) else pad_value for x in sequence]
-
-        # Pad or truncate the sequence to max_len
-        cleaned_sequence += [pad_value] * (self.max_len - len(cleaned_sequence))
-        return cleaned_sequence[:self.max_len]
+        return features_tensor, labels_tensor
 
     def __len__(self):
-        return len(self.data)
+        return len(self.normalized_data)
+
+# Usage example:
+# dataset = NTLoader(your_dataframe, max_len=50)
+# dataloader = DataLoader(dataset=dataset, batch_size=32, shuffle=True)
